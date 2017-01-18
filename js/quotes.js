0a0aa71221d8522fb875fffd7b51cc76b438a231
Willian Jonathan <willianjonathan_@hotmail.com>
2017-01-18 17:49:56 -0200

    Editions

--- js/quotes.js
+++ js/quotes.js
@@ -1,25 +1,109 @@
 $(document).ready(function(){
-   $("#getQuote").on("click",function(e){
+
+  $("#getQuote").on("click",function(e){     
+        $("#makeTweet").remove();   
+        $("#blockquote").remove(); 
+        $("#presentation").remove(); 
+        $("#father").append('<blockquote id="blockquote"><p id="quote"></p><footer id="author"></footer></blockquote>');
+        $("#button").append('<a id="makeTweet" class="btn btn-info" href="#" target="_blank">Tweet</a>');
         e.preventDefault();
                 $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?",function(json){
+
+                    if(json.quoteAuthor.length === 0){
+                          json.quoteAuthor = "Unknown";
+                    }
+
                     $("#quote").html(json.quoteText);
                     $("#author").html(json.quoteAuthor);
-                });
-    });
-    
-    $("#makeTweet").on("click",function(e){
-            e.preventDefault();
-            var text = $("#quote").text() + " - " + $("#author").text();
-            if(text.length <=140){
 
-                <a href="http://twitter.com/home?status=Custom%20status%20here" target="_blank">
-                $.post("http://twitter.com/home?status="+text,function(){
+                    if( json.quoteText.length > 0){
+                        var text = (json.quoteText + " - " + json.quoteAuthor).replace(";",".");
+                        $("#makeTweet").attr("href","http://twitter.com/home?status="+text);
+                    }
+
+                    $("#makeTweet").css("visibility","visible");
+
                 });
-            }else{
-                alert("Unfortunately the quote has more than 140 caracters, so it can't be twitted!");
-            }
-            
     });
+
 });
 
 
+
+// --------------------------------------------------------------------------------
+
+/*
+  Code by Gabriel Nunes
+*/
+
+function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
+
+var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+var currentQuote = '', currentAuthor = '';
+
+function openURL(url){
+  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+}
+
+function getQuote() {
+  $.ajax({
+    headers: {
+      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+    success: function(response) {
+      var r = JSON.parse(response);
+      currentQuote = r.quote;
+      currentAuthor = r.author;
+      if(inIframe())
+      {
+        $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+        $('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote));
+      }
+      $(".quote-text").animate({
+          opacity: 0
+        }, 500,
+        function() {
+          $(this).animate({
+            opacity: 1
+          }, 500);
+          $('#text').text(r.quote);
+        });
+
+      $(".quote-author").animate({
+          opacity: 0
+        }, 500,
+        function() {
+          $(this).animate({
+            opacity: 1
+          }, 500);
+          $('#author').html(r.author);
+        });
+
+      var color = Math.floor(Math.random() * colors.length);
+      $("html body").animate({
+        backgroundColor: colors[color],
+        color: colors[color]
+      }, 1000);
+      $(".button").animate({
+        backgroundColor: colors[color]
+      }, 1000);
+    }
+  });
+}
+$(document).ready(function() {
+  getQuote();
+  $('#new-quote').on('click', getQuote);
+  $('#tweet-quote').on('click', function() {
+    if(!inIframe()) {
+      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+    }
+  });
+  $('#tumblr-quote').on('click', function() {
+    if(!inIframe()) {
+      openURL('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote));
+    }
+  });
+});
